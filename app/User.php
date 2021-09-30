637972dc94ac55f58ababc7137b338f533700e03
agustinafassinaengworks <agustina.fassina@engworks.com>
2021-09-30 18:38:02 -0300

    first commit :D apilaravel

---
+++ app/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password'];
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /*
+        Añadiremos estos dos métodos
+    */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
